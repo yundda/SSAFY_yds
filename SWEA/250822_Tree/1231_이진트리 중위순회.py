f23051a2ef579522aed4c1cd3265a0e6be71a969
yundda <vvvzzang1218@naver.com>
2025-08-22 17:57:47 +0900

    [250822]

--- SWEA/250822_Tree/1231_이진트리 중위순회.py
+++ SWEA/250822_Tree/1231_이진트리 중위순회.py
@@ -2,33 +2,39 @@ T = 1
 for _ in range(T):
 
     N = int(input())
-    # k = 0
-
-    # def find_k(N):
-    #     global k
-    #     if N == 1:
-    #         return
-    #     k += 1
-    #     find_k(N // 2)
-
-    # find_k(N)
-    # for i in range(k):
-    #     for j in range(2**i):
-    #         node, c, L, R = (input().split())
-    #         node = int(node)
-    #         if L:
-    #             L = int(L)
-    #         if R:
-    #             R = int(R)
-    tree = [[] for _ in range(N)]
+ 
+    tree = [[] for _ in range(N + 1)]
     for _ in range(N):
         arr = list(input().split())
+        node = int(arr[0])
+        c = arr[1]
+        L = int(arr[2]) if len(arr) >=3 else 0
+        R = int(arr[3]) if len(arr) >= 4 else 0
 
-        if len(arr) == 4:
-            node, c, L, R = int(arr[0]), arr[1], int(arr[2]), int(arr[3])
-        elif len(arr) == 3:
-            node, c, L, R = int(arr[0]), arr[1], int(arr[2]), 0
-        else:
-            node, c, L, R = int(arr[0]), arr[1], 0, 0
+        tree[node].append(c)
+        tree[node].append(L)
+        tree[node].append(R)
 
-        print(node, c, L, R)
+    def inorder(node):
+        global word
+        if node == 0:
+            return
+        if node > N:
+            return
+        
+        inorder(tree[node][1] )
+        word += (tree[node][0])
+        inorder(tree[node][2] )
+    word = ''
+    inorder(1)
+    print(word)
+
+# 8
+# 1 W 2 3
+# 2 F 4 5
+# 3 R 6 7
+# 4 O 8
+# 5 T
+# 6 A
+# 7 E
+# 8 S
--- SWEA/250822_Tree/5176_이진트리 중위순회.py
+++ SWEA/250822_Tree/5176_이진트리 중위순회.py
@@ -5,19 +5,19 @@ for t in range(1, T + 1):
     tree = [0] * (N + 1)
     i = 1
 
-    def preorder(node):
+    def inorder(node):
         global i
         if node > N:
             return
         # 왼쪽
-        preorder(node * 2)
+        inorder(node * 2)
         # 현재 노드
         tree[node] = i
         i += 1
         # 오른쪽 노드
-        preorder(node * 2 + 1)
+        inorder(node * 2 + 1)
 
-    preorder(1)
+    inorder(1)
     root = tree[1]
     child = tree[N // 2]
 
